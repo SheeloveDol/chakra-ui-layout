293825bfd930f4008c92482fff8cdaac0eeca179
Sheelove Dol <siesel26@gmail.com>
2022-08-25 00:02:32 -0700

    figured out the ideal responsive layout

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import {
   VStack,
   Code,
   Grid,
+  GridItem,
+  Center,
+  Container,
+  SimpleGrid,
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
@@ -15,26 +19,89 @@ import { Logo } from './Logo';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+      <Grid
+        border='2px'
+        borderColor='red'
+        w={['375px', null, null, '1290px']}
+        
+        h={['100%', null, null, '100vh']}
+        // w={['100%', null, null, '100vw']}
+        
+        m='auto'
+        
+        
+      >
+        <Grid 
+          border='2px'
+          borderColor='blue.300'
+          h={[null, null, null, '518px']}
+          w={['375px', null, null, '1110px']}
+          templateRows={[null, null, null, 'repeat(2, 1fr)']}
+          templateColumns={[null, null, null, 'repeat(4, 1fr)']}
+          mx='auto'
+          my='auto'
+          px='auto'
+          gap='1.5rem'
+        >
+          <GridItem
+            w={['327px', null, null, '255px']}
+            rowSpan={[1, null, null, 2]}
+            h={['203px', null, null, '518px' ]}
+            background='blue.100'
+            mx='auto'
+          >
+          </GridItem>
+
+          <GridItem
+            w={['327px', null, null, '255px']}
+            h={['160px', null, null, '244px' ]}
+            border='1px'
+            mx='auto'
+          >
+          </GridItem>
+
+          <GridItem
+            w={['327px', null, null, '255px']}
+            h={['160px', null, null, '244px' ]}
+            border='1px'
+            mx='auto'
+          >
+          </GridItem>
+
+          <GridItem
+            w={['327px', null, null, '255px']}
+            h={['160px', null, null, '244px' ]}
+            border='1px'
+            mx='auto'
+          >
+          </GridItem>
+
+          <GridItem
+            w={['327px', null, null, '255px']}
+            h={['160px', null, null, '244px' ]}
+            border='1px'
+            mx='auto'
+          >
+          </GridItem>
+
+          <GridItem
+            w={['327px', null, null, '255px']}
+            h={['160px', null, null, '244px' ]}
+            border='1px'
+            mx='auto'
+          >
+          </GridItem>
+
+          <GridItem
+            w={['327px', null, null, '255px']}
+            h={['160px', null, null, '244px' ]}
+            border='1px'
+            mx='auto'
+          >
+          </GridItem>
+
         </Grid>
-      </Box>
+      </Grid>
     </ChakraProvider>
   );
 }
